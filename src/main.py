4e7cc5bdae649189206ec9d3db3366e1d877004f
Andre-UFERSA-CC <andre.vidal@alunos.ufersa.edu.br>
2026-06-04 15:45:32 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,14 @@ def main() -> None:
     amostra = [float(s) for s in input().replace(" ", "").split(",")]
     estimador = Estimador(amostra)
     estimador.mostrar_valores()
+    media_hipotese = float(input("Insira uma media hipotetica: "))
+    valor_p = estimador.valor_p_bilateral(media_hipotese)
+    print(f"Valor-p da hipótese H0 : \\mu = {media_hipotese} => {valor_p}")
+    alfa = float(input("Insira o valor de alfa: "))
+    if valor_p < alfa:
+        print("Rejeitada")
+    else:
+        print("Aceita")
     return
 
 if __name__ == "__main__":
